961ec7cb52f84284e323cb1901bc3205d0f02bd3
sarthaksavvy <sarthak@bitfumes.com>
2022-03-18 11:40:23 +0530

    29 Nextjs - add sneaker pricing design

--- pages/sneakers.tsx
+++ pages/sneakers.tsx
@@ -42,8 +42,9 @@ const Sneakers: NextPage = () => {
           {sneakers.map((sneaker) => (
             <div
               key={sneaker.name}
-              className="text-center border rounded-md shadow-sm hover:shadow-md cursor-pointer"
+              className="text-center text-gray-400 hover:text-gray-700 border rounded-md shadow-sm hover:shadow-md cursor-pointer flex flex-col justify-between p-3"
             >
+              <p className="text-right text-sm">$ {sneaker.retailPrice}</p>
               <Image
                 src={sneaker.image.thumbnail}
                 alt={sneaker.name}
@@ -51,7 +52,7 @@ const Sneakers: NextPage = () => {
                 height="200"
                 className="rounded-lg shadow-md"
               />
-              <p className="font-semibold">{sneaker.name}</p>
+              <p className="text-sm bottom-0">{sneaker.name}</p>
             </div>
           ))}
         </div>
