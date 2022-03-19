97d41e671cd0d6ed31badecf09ac08d188fab086
sarthaksavvy <sarthak@bitfumes.com>
2022-03-19 10:06:43 +0530

    34 Nextjs - design single sneaker

--- pages/sneakers/[brand]/[sneaker_id].tsx
+++ pages/sneakers/[brand]/[sneaker_id].tsx
@@ -1,3 +1,4 @@
+import Layout from "components/Layout";
 import db from "db.json";
 import Image from "next/image";
 import { useRouter } from "next/router";
@@ -13,21 +14,43 @@ export default function Sneaker() {
   }, [query]);
 
   return (
-    <div>
+    <Layout>
       {sneaker?.name && (
-        <>
-          <p>{sneaker?.name}</p>
-          <p>{sneaker?.brand}</p>
-          <p>{sneaker?.story}</p>
-          <p>{sneaker?.retailPrice}</p>
+        <div className="grid grid-cols-2 gap-4">
           <Image
-            src={sneaker?.image?.small}
-            width="200"
-            height="200"
+            src={sneaker?.image?.original}
+            width="100%"
+            height="100%"
+            layout="responsive"
             alt={sneaker.name}
           />
-        </>
+          <div>
+            <p className="text-2xl text-gray-600">Brand: {sneaker?.brand}</p>
+            <p className="text-3xl mt-4">{sneaker?.name}</p>
+
+            {sneaker?.story && (
+              <div className="text-gray-600 my-4">
+                <p className="font-semibold">Description:</p>
+                <p className="text-gray-500">{sneaker?.story}</p>
+              </div>
+            )}
+
+            <div className="text-gray-600 my-4 flex">
+              <p className="font-semibold">Gender:</p>
+              <p className="ml-2">{sneaker.gender}</p>
+            </div>
+
+            <div className="flex">
+              <p className="text-xl align-middle py-2">
+                $ {sneaker?.retailPrice}
+              </p>
+              <button className="px-2 w-40 py-2 rounded-md shadow bg-indigo-800 text-white ml-4">
+                Add to Card
+              </button>
+            </div>
+          </div>
+        </div>
       )}
-    </div>
+    </Layout>
   );
 }
