5dada29c61d98de512c4ba618e065e6b49295929
sarthaksavvy <sarthak@bitfumes.com>
2022-04-10 12:34:36 +0530

    59 Nextjs - next auth credentials

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
 
 export default NextAuth({
@@ -6,6 +7,31 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Email Address",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Your password",
+        },
+      },
+      async authorize(credentials, req) {
+        // need to fetch user details from database
+        const user = {
+          id: 1,
+          email: "sarthak@mezohub.com",
+          name: "Sarthak",
+        };
+        return user;
+        return null;
+      },
     }),
   ],
 });
