622464f197d659071208483c656a645cc36769ed
sarthaksavvy <sarthak@bitfumes.com>
2022-05-10 11:36:18 +0530

    63 Nextjs - get server side props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,10 @@ import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import "../styles/global.css";
 
-function MyApp({ Component, pageProps: { session, pageProps } }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </SessionProvider>
   );
 }
--- pages/sneakers/index.tsx
+++ pages/sneakers/index.tsx
@@ -1,24 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
 import Layout from "components/Layout";
 import ShowSneakers from "components/ShowSneakers";
-import { NextPage } from "next";
-import React, { useEffect, useState } from "react";
+import { GetServerSideProps, NextPage } from "next";
+import React from "react";
 import { Sneaker } from "types/sneakers";
 
-const Sneakers: NextPage = () => {
-  const [sneakers, setsneakers] = useState<Sneaker[]>([]);
-
-  useEffect(() => {
-    fetch("/api/sneakers")
-      .then((res) => res.json())
-      .then((result: Sneaker[]) => setsneakers(result));
-  }, []);
-
+const Sneakers: NextPage<{ sneakers: Sneaker[] }> = ({ sneakers }) => {
   return (
     <Layout>
-      <ShowSneakers sneakers={sneakers} setsneakers={setsneakers} />
+      <ShowSneakers sneakers={sneakers} />
     </Layout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const data = await fetch("http://localhost:3000/api/sneakers");
+  const results = await data.json();
+
+  return {
+    props: { sneakers: results },
+  };
 };
 
 export default Sneakers;
